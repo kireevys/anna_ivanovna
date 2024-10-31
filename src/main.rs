1859a1882c6efcc8cafea0255d12f78875b87762
kireev <kireev_y@tochka.com>
2024-11-01 00:02:01 +0300

    --ammend

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 use anna_ivanovna::cli;
 use anna_ivanovna::storage::plan_from_yaml;
-use std::path::Path;
-use std::{env, panic};
+use std::env;
 
 fn main() {
-    let path = match env::current_dir() {
-        Ok(path) => path.join(Path::new("storage/plan.yaml")),
-        Err(e) => panic!("Error retrieving current directory: {e}"),
-    };
+    let path = env::current_dir().expect("Failed to get current directory");
     assert!(path.exists(), "Не найден файл: {}", path.display());
     println!("Используется файл плана {}", path.display());
     let plan = plan_from_yaml(path.as_path());
