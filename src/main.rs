74fa8e86633be9d541145ab850315e9b67752a7e
kireev <kireev_y@tochka.com>
2025-07-03 20:14:50 +0300

    delete win  PS

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,3 @@
-#[cfg(windows)]
-fn main() {
-    use std::env;
-    use std::process::Command;
-
-    // Проверяем, есть ли переменная окружения, указывающая на терминал
-    let is_terminal = env::var("TERM").is_ok() || env::var("WT_SESSION").is_ok();
-
-    if !is_terminal {
-        // Открываем PowerShell с командой help
-        let exe = env::current_exe().unwrap();
-        Command::new("powershell")
-            .arg("-NoExit")
-            .arg("-Command")
-            .arg(format!("{} --help; pause", exe.display()))
-            .spawn()
-            .unwrap();
-        return;
-    }
-
-    anna_ivanovna::cli::run().unwrap();
-}
-
-#[cfg(not(windows))]
 fn main() {
     anna_ivanovna::cli::run().unwrap();
 }
