73f4aeb59bb29c6e971ebc85f2c37f1421a9577c
kireev <kireev_y@tochka.com>
2026-04-04 16:27:45 +0300

    fix(logs): logs rotating now

--- backend/src/infra/logging.rs
+++ backend/src/infra/logging.rs
@@ -1,18 +1,22 @@
-use std::{
-    fs::{OpenOptions, create_dir_all},
-    path::Path,
-};
+use std::{fs::create_dir_all, path::Path};
+
+use tracing_appender::rolling::{RollingFileAppender, Rotation};
 use tracing_subscriber::fmt::writer::MakeWriterExt;
 
 pub fn init(dir: &Path, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
     let logdir = dir.join("logs");
-    let logfile = logdir.join(filename);
     if let Err(e) = create_dir_all(&logdir) {
         eprintln!("Не удалось создать директорию для логов: {e}");
         std::process::exit(1);
     }
-    let file = OpenOptions::new().create(true).append(true).open(logfile)?;
-    let writer = std::io::stderr.and(file);
+
+    let file_appender = RollingFileAppender::builder()
+        .rotation(Rotation::DAILY)
+        .filename_prefix(filename)
+        .max_log_files(7)
+        .build(&logdir)?;
+
+    let writer = std::io::stderr.and(file_appender);
     tracing_subscriber::fmt()
         .with_writer(writer)
         .with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE)
